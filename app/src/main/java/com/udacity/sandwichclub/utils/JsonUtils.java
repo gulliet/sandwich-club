a2ecaf135e5597b06b3fb8f4ef8eb2a2412e1b40
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2018-02-18 10:37:42 +0100

    Create the main JSON sandwich object

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,34 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class JsonUtils {
 
+    private static final String TAG = "JsonUtils";
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        Sandwich parsedSandwich = null;
+
+        if (json == null || json.isEmpty()) {
+            Log.d(TAG, "Empty JSON String");
+            return null;
+        } else {
+            Log.d(TAG, "JSON String to be parsed: " + json);
+        }
+
+        try {
+            JSONObject sandwichJSON = new JSONObject(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return parsedSandwich;
     }
 }
